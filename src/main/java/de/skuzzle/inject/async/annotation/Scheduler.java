c47a4d25767bb562425213995ec7ebfc67376876
Simon Taddiken <simon.taddiken@gmail.com>
2016-04-07 22:21:27 +0200

    Add annotations

--- src/main/java/de/skuzzle/inject/async/annotation/Scheduler.java
+++ src/main/java/de/skuzzle/inject/async/annotation/Scheduler.java
@@ -1,7 +1,13 @@
 package de.skuzzle.inject.async.annotation;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 import java.util.concurrent.ScheduledExecutorService;
 
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
 public @interface Scheduler {
     Class<? extends ScheduledExecutorService> value();
 }
